6ab4f67dedea82f55c77dea76e75a87b30ecdbac
praveenmethraskar <praveenmethraskar@gmail.com>
2022-08-05 16:39:13 +0530

    First Use Case

---
+++ SnakeAndLadders/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SnakeAndLadders
+{
+    public class SnakeAndLadder
+    {
+        
+        public void Input()
+        {
+
+            Console.WriteLine("Snake and Ladder Game Problem");
+        }
+        
+    }
+
+    public class SnakeAndLadder_Child : SnakeAndLadder
+    {
+        public void display()
+        {
+            Random random = new Random();
+
+            int Check_Snake_or_Ladder = random.Next(0, 2);
+
+
+            int position = 0;
+            if (position == 0)
+            {
+                Console.WriteLine("single plyer with 0th position");
+            }
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            SnakeAndLadder_Child obj = new SnakeAndLadder_Child();
+            obj.display();
+        }
+    }
+}
